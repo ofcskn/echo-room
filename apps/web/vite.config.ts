d2acd683509566ff4361150eb3f770c4b1546de9
Ömer Faruk Coşkun <ofcskn.omer@gmail.com>
2025-12-14 14:13:39 +0300

    feat(web): Allowed hosts are added.

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,13 +1,17 @@
+import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const allowedHosts = env.VITE_ALLOWED_HOSTS
+    ? env.VITE_ALLOWED_HOSTS.split(',').map(h => h.trim())
+    : []
     return {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts,
       },
       plugins: [react()],
       define: {
